0c4d0e7c7f7e3e1e998deb210684aae447ab63f2
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-20 16:40:58 -0300

    Player Dto

--- src/main/java/joice/lima/uolhostbackend/controller/PlayerController.java
+++ src/main/java/joice/lima/uolhostbackend/controller/PlayerController.java
@@ -1,7 +1,9 @@
 package joice.lima.uolhostbackend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +20,10 @@ public class PlayerController {
     @Autowired
     private PlayerService service;
 
+    @PostMapping
     public ResponseEntity<Player> createPlayer(@RequestBody @Valid PlayerDto dto){
         Player newPlayer = service.createPlayer(dto);
+        return new ResponseEntity<>(newPlayer, null, HttpStatus.CREATED);
     }
 
     }
