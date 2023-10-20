d07450ac124d9baf6a8470115897abec88f1c8c1
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-20 15:15:52 -0300

    Initial Project

---
+++ src/test/java/joice/lima/uolhostbackend/UolhostBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package joice.lima.uolhostbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolhostBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
