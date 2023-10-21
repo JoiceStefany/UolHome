077f004e5e9ecccdb6c0baf69e0b046b2dffbfb5
JoiceStefany <steffanyjoice006@gmail.com>
2023-10-21 16:01:18 -0300

    Condiname Handler

---
+++ src/main/java/joice/lima/uolhostbackend/infra/CodinameHandler.java
@@ -0,0 +1,12 @@
+package joice.lima.uolhostbackend.infra;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CodinameHandler {
+    
+     
+
+
+
+}
--- src/main/java/joice/lima/uolhostbackend/service/CodinameService.java
+++ src/main/java/joice/lima/uolhostbackend/service/CodinameService.java
@@ -1,9 +1,14 @@
 package joice.lima.uolhostbackend.service;
 
+import java.util.ArrayList;
+
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Getter;
 
@@ -16,4 +21,17 @@ public class CodinameService {
 
     @Autowired
     private Environment env;
+
+    private List<String> avangersCondinameList = new ArrayList<>();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    public void loadJsonData(){
+        
+        try {
+            String condinameResponse = restTemplate.getForObject(env.getProperty("avangers"), String.class);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
